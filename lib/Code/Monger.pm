ca540076ef4745f0f70d6e0fbaf157df85a00ed3
Gabor Szabo <gabor@szabgab.com>
2014-09-10 00:15:04 +0300

    start code

---
+++ lib/Code/Monger.pm
@@ -0,0 +1,15 @@
+package Code::Monger;
+use Moose;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Code::Monger - Doing something
+
+=cut
+
+
+
+1;
+
---
+++ t/00-load.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+use Code::Monger;
+
+ok 1;
