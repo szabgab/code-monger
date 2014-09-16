2d6c0488f103405837b1c9a6c27244cd7411c5b3
Gabor Szabo <gabor@szabgab.com>
2014-09-16 12:02:46 +0300

    use PPIx::EditorTools

--- lib/Code/Monger/Perl.pm
+++ lib/Code/Monger/Perl.pm
@@ -1,24 +1,29 @@
 package Code::Monger::Perl;
 use Moose;
 
+use PPIx::EditorTools::Outline;
+
 sub process {
 	my ( $self, $code ) = @_;
 
-	my @subs      = $code =~ m{sub\s+(\w+)}g;
-	my @loaded    = $code =~ m{use\s+([\w:]+)}g;
-	my @modules   = grep { $_ ne lc $_ } @loaded;
-	my @pragmatas = grep { $_ eq lc $_ } @loaded;
+	my $ppi = PPIx::EditorTools::Outline->new;
+	return $ppi->find( code => $code );
 
-	my @calls = $code =~ m{(\w+)\s*\(}g;
+	#my @subs      = $code =~ m{sub\s+(\w+)}g;
+	#my @loaded    = $code =~ m{use\s+([\w:]+)}g;
+	#my @modules   = grep { $_ ne lc $_ } @loaded;
+	#my @pragmatas = grep { $_ eq lc $_ } @loaded;
 
-	my %data = (
-		subs      => \@subs,
-		modules   => \@modules,
-		pragmatas => \@pragmatas,
-		calls     => \@calls,
-	);
+	#my @calls = $code =~ m{(\w+)\s*\(}g;
 
-	return \%data;
+	#my %data = (
+	#	subs      => \@subs,
+	#	modules   => \@modules,
+	#	pragmatas => \@pragmatas,
+	#	calls     => \@calls,
+	#);
+
+	#return \%data;
 }
 
 1;
--- t/01-perl.t
+++ t/01-perl.t
@@ -9,14 +9,47 @@ my $cm = Code::Monger->new;
 $cm->process('t/corpus/eg/Abc.pm');
 
 #diag explain $cm->data;
-is_deeply $cm->data, {
-	'pragmatas' => [ 'strict', 'warnings', ],
-	'modules'   => [ 'Bob', ],
-	'subs' => [ 'foo', 'bar', '_quix', ],
-	calls  => [
+is_deeply $cm->data,
+	[
+	{
+		'name'    => 'Abc',
+		'methods' => [
+			{
+				'line' => 8,
+				'name' => 'foo'
+			},
+			{
+				'name' => 'bar',
+				'line' => 11
+			},
+			{
+				'line' => 16,
+				'name' => '_quix'
+			}
+		],
+		'line'    => 1,
+		'modules' => [
+			{
+				'line' => 5,
+				'name' => 'Bob'
+			}
+		],
+		'pragmata' => [
+			{
+				'name' => 'strict',
+				'line' => 2
+			},
+			{
+				'line' => 3,
+				'name' => 'warnings'
+			}
+		]
+	}
+	]
 
-		# TODO: 'foo'
-		'_quix',
-	],
-};
+	#calls  => [
+
+	#	# TODO: 'foo'
+	#	'_quix',
+	#],
 
